36bd22802f50c1cb7a1789b99c5b5fb02ef747b2
tarousse <58089993+azizetrou@users.noreply.github.com>
2020-01-07 18:42:57 +0100

    Add files via upload

---
+++ page/news/‫php2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../font-awesome/css/all.css">
+    <link rel="stylesheet" href="ppp/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <a class="menu" onclick="menu()"><i class="fas fa-bars"></i></a>
+    <div class="menu-bar" id="menu1">
+        <a href="../../index.php">الرئيسية <i class="fas fa-home"></i></a>
+        <a href="../news.php">أخبار <i class="fas fa-newspaper"></i></a>
+        <a href="../articles.php">مقالات <i class="far fa-newspaper"></i></a>
+        <a href="../reports.php">تقارير <i class="far fa-clipboard"></i></a>
+        <a href="../press.php">بيانات صحفية <i class="fas fa-camera"></i></a>
+        <a href="../contact.php">تواصل معنا <i class="fas fa-address-book"></i></a>
+    </div>
+    
+    <?php include "news-bar.inc.php";?>
+
+    <div class="paragraph">
+        <h1>
+         حوامل بانتون   andrea antoni
+              </h1>
+
+    
+        <strong>
+        </strong>
+        <p>
+        andrea antoni ، المعروف أيضًا باسم "Style1" و "Stailuan" على الشبكات الاجتماعية ، هو فنان إيطالي يأتي من Monfalcone. وهو مصمم جرافيك مستقل وكاتب على الجدران.
+         </p>
+        <p>
+        نركز اليوم على لقطات حساب Instagram التي تمثل بعض التطابقات بين المناظر الطبيعية وحوامل Pantone التي يحملها بيد واحدة. هذه المباريات هي جزء من مشروع #STAILtone الذي بدأه الفنان في نوفمبر 2014. من خلال أعماله الفنية ، يريد Andrea فقط "مشاركة الحالة المزاجية أو الشعور بالألوان والتركيب".
+
+        </p>
+        <p>
+        «في بعض الأحيان ، تكون اللقطة حقيقية: أحمل حامل Pantone في إحدى يديها ، وكاميرا الإثارة في جهة أخرى وألتقط صورة. في أوقات أخرى ، التقطت صورة وأضيف حامل Pantone في مرحلة ما بعد الإنتاج. " هو قال. «في بعض الأحيان ، لا أقوم سوى بإضافة حامل اللون لأنني أرغب في إظهار ألوان المناظر الطبيعية كما هي. في أوقات أخرى ، قررت استخدام لون معين وأربطه مع صورة أقوم بتعديلها بعد ذلك. يمكنني أيضًا التقاط صورة بمنظور يساعدني في إنشاء لعبة من الانعكاسات والألوان في مرحلة ما بعد الإنتاج. " هو يضيف. «دعنا نقول أن عملي الفني يعتمد على مشاعري العميقة وكيف تسير أيامي. ليس لدي خطة تحريرية محددة أو خط إنتاج فريد ، فأنا حريص على عدم نشر طلقتين بنفس اللون واحدة تلو الأخرى. »أخبرنا أخيرًا.
+
+        </p>
+        <strong>
+        نحن نحبها !
+        </strong>
+        
+        <img src="image/1.jpg">
+        <img src="image/2.jpg">
+        <img src="image/3.jpg">
+        <img src="image/4.jpg">
+        <img src="image/5.jpg">
+        <img src="image/6.jpg">
+        <img src="image/7.jpg">
+        <img src="image/8.jpg">
+       
+    </div>
+
+     <span class="aside-bar"><?php include "aside.inc.php";?></span>
+    <?php include "footer.inc.php";?>
+    <script src="../../script.js"></script>
+
+</body>
+</html>
