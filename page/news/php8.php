ce88c208f3c711cc76ef9d213423f4c823a7d34a
tarousse <58089993+azizetrou@users.noreply.github.com>
2020-01-14 21:29:24 +0100

    Add files via upload

---
+++ page/news/php8.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../font-awesome/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <a class="menu" onclick="menu()"><i class="fas fa-bars"></i></a>
+    <div class="menu-bar" id="menu1">
+        <a href="../../index.php">الرئيسية <i class="fas fa-home"></i></a>
+        <a href="../news.php">أخبار <i class="fas fa-newspaper"></i></a>
+        <a href="../articles.php">مقالات <i class="far fa-newspaper"></i></a>
+        <a href="../reports.php">تقارير <i class="far fa-clipboard"></i></a>
+        <a href="../press.php">بيانات صحفية <i class="fas fa-camera"></i></a>
+        <a href="../contact.php">تواصل معنا <i class="fas fa-address-book"></i></a>
+    </div>
+
+
+
+    <?php include "news-bar.inc.php";?>
+    <div class="paragraph">
+       <strong>
+        </strong>
+        <h1>
+تأخرت شركة Verizon في طرح الجيل الخامس في المنزل مرة أخرى بسبب الحاجة إلى معدات جديدة
+
+        </h1>
+        <img src="image/.jpg" alt="">
+        <p>
+تقوم Verizon بتأخير طرح خدمة 5G في المنزل مرة أخرى.  قال مسؤول تنفيذي في Verizon لـ Light Reading إنه ينتظر معدات جديدة وأكثر قوة من المفترض أن يتم إصدارها في النصف الثاني من هذا العام قبل أن توسع الخدمة.
+        </p>
+        <p>
+
+  تمتاز خدمة 5G Home من Verizon بتاريخ متقلب. أطلقت الشركة لأول مرة شركة 5G في أربع مدن أمريكية في أكتوبر 2018 ، والتي ادعت أنها جعلتها أول شركة طيران أمريكية تطرح 5G. لكن النسخة الأولى من 5G Home كانت عبارة عن 5G فقط لأنها استخدمت تقنية 5G الخاصة بشركة Verizon والتي تسمى 5G TF بدلاً من معيار 5G NR الأكثر استخدامًا على نطاق واسع ، والذي يستخدم لكل من خدمات الجوال ذات النطاق المنخفض و mmWave 5G بواسطة كل شركة اتصالات جوال أخرى ( بما في ذلك شبكة 5G المحمول فيريزون).
+
+        </p>
+        <h2 style="color: #ff6348; border-top:1px solid #111; margin:5px;">
+          ليست هذه هي المرة الأولى التي تؤخر فيها شركة VERIZON 5G HOME
+        </h2>
+        <p>
+  ليست هذه هي المرة الأولى التي تؤخر فيها شركة Verizon طرح تطبيق 5G Home. في يناير الماضي ، قالت Verizon أن 5G Home لن تتوسع إلى مدن أخرى حتى وقت ما في النصف الثاني من عام 2019 لأنها كانت تنتظر معدات جديدة. تبدو مألوفة؟
+</p>
+        <p>
+
+  في أكتوبر 2019، وانتهت فيريزون تصل استئناف 5G الرئيسية في سوق واحدة جديدة ، وشيكاغو، وهذا التكرار من 5G الرئيسية لا تستخدم معيار 5G NR، على الأقل. وإلا ، فإن الخدمة ، بعد أكثر من عام من إطلاقها الرسمي ، لا تزال موجودة في خمسة أسواق فقط في الولايات المتحدة.
+
+        </p>
+        <p>
+
+  صرح المسؤول التنفيذي لشركة Verizon لـ Light Reading بأنه يريد في نهاية المطاف أن تغطي شركة 5G Home حوالي 30 مليون أسرة أمريكية خلال السنوات الخمس إلى السبع القادمة. لذا فقد تنتظر بعض الوقت حتى تصل الخدمة إلى منطقتك ، إذا حدث ذلك بالفعل.
+
+        </p>
+
+    </div>
+
+  </body>
+    <span class="aside-bar"><?php include "aside.inc.php";?></span>
+    <?php include "footer.inc.php";?>
+    <script src="../../script.js"></script>
+
+
+
+
+</html>
---
+++ page/news/php9.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../font-awesome/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <a class="menu" onclick="menu()"><i class="fas fa-bars"></i></a>
+    <div class="menu-bar" id="menu1">
+        <a href="../../index.php">الرئيسية <i class="fas fa-home"></i></a>
+        <a href="../news.php">أخبار <i class="fas fa-newspaper"></i></a>
+        <a href="../articles.php">مقالات <i class="far fa-newspaper"></i></a>
+        <a href="../reports.php">تقارير <i class="far fa-clipboard"></i></a>
+        <a href="../press.php">بيانات صحفية <i class="fas fa-camera"></i></a>
+        <a href="../contact.php">تواصل معنا <i class="fas fa-address-book"></i></a>
+    </div>
+    <?php include "news-bar.inc.php";?>
+
+    <div class="paragraph">
+      <img src="image/15.jpg" alt="">
+  <h1 style="color: #ff6348;">
+أفضل شاحن لاسلكي لعام
+2020
+</h1>
+<p>
+توصيل كابل الطاقة لشحن جهاز محمول؟ كيف القرن 20 من أنت! في هذه الأيام ، وبفضل قوة الكهرومغناطيسية ، يقوم كل الأطفال الرائعين بشحن أدواتهم لاسلكيًا: قم بإسقاط هاتفك الذكيأعلى الشاحن اللاسلكي وسيبدأ تشغيله على الفور - لا توجد كابلات مطلوبة.
+</p>
+
+<p>
+
+  للعثور على أفضل أجهزة الشحن اللاسلكية ، قضينا أكثر من 40 ساعة في اختبار 14 منهم ، وذلك لشحن هاتف iPhone XS و Samsung Note 8 الذكي. أفضل اختيار لشحن حامل اللاسلكي هوYootech X2 ، وهو شاحن بسيط ومنخفض التكلفة يوفر شحن سريع ومرن. إذا كنت تبحث عن لوحة شحن منخفضة التكلفة بسعر معقول ، فإن شاحن RAVPower RP-PC066 اللاسلكي السريع ( متوفر في Amazon ) هو خيار رائع.
+
+</p>
+
+<h3>
+  هذه هي أفضل أجهزة الشحن اللاسلكي التي اختبرناها في المرتبة ، بالترتيب:
+</h3>
+<li>Yootech X2 Wireless Charging Stand</li>
+<li>RAVPower RP-PC066 Wireless Charging Pad</li>
+<li>Nimble Wireless Stand</li>
+<li>Tylt Twisty</li>
+<li> RAVPower Wireless Charging Stand</li>
+<li>Tylt Puck</li>
+<li> Aukey Graphite Lite Q 10W Wireless Fast Charger</li>
+<li>Anker PowerWave Stand</li>
+<li>Yootech F500 Wireless Charging Pad</li>
+<li>Nimble Wireless Pad</li>
+<li>Aukey 10W Wireless Fast Charging Stand</li>
+<li>Belkin Wireless Charging Stand</li>
+<li>Belkin Bold Wireless Charging Pad</li>
+    </div>
+
+    <span class="aside-bar"><?php include "aside.inc.php";?></span>
+    <?php include "footer.inc.php";?>
+    <script src="../../script.js"></script>
+
+</body>
+
+</html>
