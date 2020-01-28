e2b3cffd3e9b3d2a2b37155638a35a642fc49d7a
tarousse <58089993+azizetrou@users.noreply.github.com>
2020-01-28 22:08:13 +0100

    Add files via upload

---
+++ page/news/php13.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+  <a class="menu" onclick="menu()"><i class="fas fa-bars"></i></a>
+  <div class="menu-bar" id="menu1">
+      <a href="../../index.php">الرئيسية <i class="fas fa-home"></i></a>
+      <a href="../news.php">أخبار <i class="fas fa-newspaper"></i></a>
+      <a href="../articles.php">مقالات <i class="far fa-newspaper"></i></a>
+      <a href="../reports.php">تقارير <i class="far fa-clipboard"></i></a>
+      <a href="../press.php">بيانات صحفية <i class="fas fa-camera"></i></a>
+      <a href="../contact.php">تواصل معنا <i class="fas fa-address-book"></i></a>
+  </div>
+  <?php include "news-bar.inc.php";?>
+  <div class="paragraph">
+<img src="image/19.jpg" alt="">
+<h1>
+أفضل قفازات لمس من 2019
+</h1>
+<p>
+بعد أن أمضيت سنوات في مناخ شتوي ، لم أجد بعدًا قفازًا شتويًا جيدًا يمكنني استخدامه مع الهاتف الذكي. 
+    </p><p>
+    رأيت طماق التي ترفض مزق والبطانيات الحد من القلق ، ولكن أين هي القفازات التي يمكن أن تبقي أصابعك دافء بشكل ممتع مع الرسائل النصية؟ لذلك ، قمت بتجميع أفضل قفازات الشاشة التي تعمل باللمس في السوق ووضعها من خلال عملية الاختبار الصارمة لمراجعة.
+</p>
+<p>
+  بعد أسابيع من أسابيع من الاختبارات - وبعد قتال كرة الثلج في وقت لاحق - وجدنا أن Harrms Leather Gloves ( متوفر في Amazon مقابل 27.99 دولارًا ) هو أفضل اختيار لدينا بسبب تصميمه الأنيق ومواده الدافئة والمريحة.
+    </p><p>
+    إذا كنت تبحث عن قفازات تناسب الجلد الثاني ، فإن قفازات North Face Etip (المتوفرة في Amazon مقابل 44.95 دولارًا) تناسبك. ولكن إذا لم تكن هذه الاختيارات هي كوب الشاي ، فلا تقلق ، فلدينا العديد من الخيارات الأخرى.
+</p>
+<p>
+  هذه هي أفضل قفازات تعمل باللمس اختبرناها في المرتبة ، من أجلك:
+</p>
+<p>
+  <ol style="direction:ltr;">
+    <li>	Harrms Leather Gloves</li>
+    <li>	North Face Etip Gloves</li>
+    <li>	Black Diamond HeavyWeight Screentap Gloves</li>
+    <li>	Mujjo Double Layed Knitted Touchscreen Winter Gloves</li>
+    <li>	Achiou Winter Knit Touchscreen Gloves</li>
+    <li>	GliderGloves Copper Infused Touchscreen Gloves</li>
+    <li>	Mujjo Insulated Winter Touchscreen Gloves with 3M Thinsulate</li>
+    <li>	Moshi Digits Touchscreen Gloves</li>
+    <li>	Ozero Leather Gloves</li>
+    <li>	Warmen Touchscreen Texting Nappa Leather Gloves</li>
+    <li>	Bruceriver Pure Wool Knitted Gloves</li>
+  </ol>
+</p>
+</div>
+<span class="aside-bar"><?php include "aside.inc.php";?></span>
+<?php include "footer.inc.php";?>
+<script src="../../script.js"></script>
+
+</body>
+
+</html>
