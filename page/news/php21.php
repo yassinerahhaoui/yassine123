609d294fe1f8435ccd38602fd08abd70ce3f6f53
tarousse <58089993+azizetrou@users.noreply.github.com>
2020-02-14 14:45:28 -0800

    Add files via upload

---
+++ page/news/php21.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<meta name="descrption" content="نظرًا لأن الهواتف القابلة للطي لا تزال غير شائعة ، يرغب الكثير من المشترين المحتملين في معرفة مدى متانتها قبل تسديد الأموال"  >
+
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="../../font-awesome/css/all.css">
+<title>
+الهواتف القابلة للطي لا تزال غير شائعة
+</title>
+</head>
+
+<body>
+
+<a class="menu" onclick="menu()"><i class="fas fa-bars"></i></a>
+
+<div class="menu-bar" id="menu1">
+
+<a href="../../index.php">الرئيسية <i class="fas fa-home"></i></a>
+
+<a href="../news.php">أخبار <i class="fas fa-newspaper"></i></a>
+
+<a href="../articles.php">مقالات <i class="far fa-newspaper"></i></a>
+
+<a href="../reports.php">تقارير <i class="far fa-clipboard"></i></a>
+
+<a href="../press.php">بيانات صحفية <i class="fas fa-camera"></i></a>
+
+<a href="../contact.php">تواصل معنا <i class="fas fa-address-book"></i></a>
+
+</div>
+
+<?php
+include "news-bar.inc.php";
+
+?>
+<div class="paragraph">
+<h1>
+مفصل موتورولا رازر "كسر" بعد 27000 طيات في اختبار المتانة
+
+</h1>
+<img src="img/52.jpg" >
+<p>
+نظرًا لأن الهواتف القابلة للطي لا تزال غير شائعة ، يرغب الكثير من المشترين المحتملين في معرفة مدى متانتها قبل تسديد الأموال مقابل واحدة - خاصة وأن الطرز المتاحة ليست رخيصة الثمن. CNET وقد وضع جديد موتورولا رازر لل اختبار من قبل للطي مرارا وتكرارا لآلاف المرات باستخدام جهاز يسمى Foldbot التي سكوير. النتيجة؟ بدأت مفصليها تتصرف متزعزع بنحو 27000 أضعاف. لاحظت دراسة أجريت عام 2017 أن الأميركيين يفحصون هواتفهم 80 مرة في اليوم. هذا يعني أن الرازر يمكن أن يبدأ في إظهار المشاكل في غضون عام.
+</p>
+<video src="https://youtu.be/epkKHwk4EMg" >
+</video>
+<p>
+في العام الماضي ، أجرت CNET   نفس الاختبار على Samsung Galaxy Fold ، والذي استمر لمدة 14 ساعة (119،380 folds) على الجهاز. تم تصميم Foldbot بالفعل لاختبار جهاز Samsung ، ولكن تم تعديل المنشور لصالح جهاز Razr ، بهدف الوصول إلى 100،000 مرة. ومع ذلك ، كان على مضيفي البث المباشر أن يختزلوا الاختبار عندما بدأ مفصل الهاتف في إصدار أصوات وإظهار مقاومة بعد حوالي أربع ساعات. بدا أيضًا غير متساوٍ وخالٍ من المحاذاة عند إغلاق الهاتف.
+</p>
+<p>
+ومع ذلك ، لا تزال شاشة رازر تعمل وتبدو جيدة حتى بعد أن بدأت مفصلاتها في العطاء. تم كسر شاشة Galaxy Fold في نهاية الاختبار ، ولكن مرة أخرى ، استمرت لفترة أطول وتم معايرة الجهاز لطيها طوال الوقت. لاختبار رازر ، طوى الجهاز الجهاز فقط في منتصف الطريق.
+</p>
+</div>
+
+<span class="aside-bar"><?php include "aside.inc.php";?></span>
+
+<?php include "footer.inc.php";?>
+
+<script src="../../script.js"></script>
+
+</body>
+
+</html>
